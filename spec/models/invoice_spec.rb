d695abe7db9c2287634bd2eea9fdbe8081b8ae4b
Shane Galvin <Sgalvin36@gmail.com>
2024-09-10 16:50:51 -0600

    Outline for _spec files

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,6 @@
+require "rails_helper"
+
+RSpec.describe Invoice, type: :model do
+    describe "instance methods" do
+    end
+end
---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,6 @@
+require "rails_helper"
+
+RSpec.describe Transaction, type: :model do
+    describe "instance methods" do
+    end
+end
