2a8f352421eeec6fb72e8a5351db531a700885cc
Kyle Delaney <khdelaney99@gmail.com>
2024-09-11 15:49:14 -0400

    test: add rspec test for get all merchants

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'simplecov'
-require 'support/factory_bot.rb'
 SimpleCov.start 'rails'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+require 'support/factory_bot.rb'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
