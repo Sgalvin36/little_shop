fa919aefbe44645c37344709322ac28c3c32d0b0
Kyle Delaney <khdelaney99@gmail.com>
2024-09-12 06:20:36 -0400

    refactor: move variables to before each block

--- spec/requests/v1/merchants_request_spec.rb
+++ spec/requests/v1/merchants_request_spec.rb
@@ -1,55 +1,58 @@
 require "rails_helper"
 
 RSpec.describe Merchant do
-    describe "instance methods" do
-        describe "#index" do
-            it "returns all merchant objects" do
-                merchant1 = Merchant.create(name: "Skippy")
-                merchant2 = Merchant.create(name: "Pippy")
-                merchant3 = Merchant.create(name: "Flippy")
-                nameArray = ["Skippy", "Pippy", "Flippy"]
+    before(:each) do
+        @merchant1 = Merchant.create(name: "Skippy")
+        @merchant2 = Merchant.create(name: "Pippy")
+        @merchant3 = Merchant.create(name: "Flippy")
+    end
+    describe "#index" do
+        it "returns all merchant objects" do
 
-                get "/api/v1/merchants"
+            nameArray = ["Skippy", "Pippy", "Flippy"]
 
-                expect(response).to be_successful
-                
-                allMerchants = JSON.parse(response.body, symbolize_names: true)
-                expect(allMerchants[:data].length).to eq(3)
-                allMerchants[:data].each do |merchant|
-                    expect(merchant).to have_key(:id)
-                    expect(nameArray).to include(merchant[:attributes][:name])
-                end
-            end
+            get "/api/v1/merchants"
 
-            it "returns a sorted list of all merchants newest to oldest" do
-                merchant1 = Merchant.create(name: "Skippy")
-                merchant2 = Merchant.create(name: "Pippy")
-                merchant3 = Merchant.create(name: "Flippy")
-                
-
-                get "/api/v1/merchants?sort=desc"
-
-                expect(response).to be_successful
-                sortedMerchants = JSON.parse(response.body, symbolize_names: true)
-                expect((sortedMerchants[:data][0][:id]).to_s).to eq(merchant3[:id].to_s)
-                expect((sortedMerchants[:data][2][:id]).to_s).to eq(merchant1[:id].to_s)
+            expect(response).to be_successful
+            
+            allMerchants = JSON.parse(response.body, symbolize_names: true)
+            expect(allMerchants[:data].length).to eq(3)
+            allMerchants[:data].each do |merchant|
+                expect(merchant).to have_key(:id)
+                expect(nameArray).to include(merchant[:attributes][:name])
             end
         end
 
-        describe "#create" do
-            it "creates a new merchant" do 
-                merchant_params = {
-                    name: "Joe"
-                }
-                headers = { "CONTENT_TYPE" => "application/json" }
+        it "returns a sorted list of all merchants newest to oldest" do
 
-                post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
+            get "/api/v1/merchants?sort=desc"
 
-                created_merchant = Merchant.last
-                
-                expect(response).to be_successful
-                expect(created_merchant.name).to eq("Joe")
-            end
+            expect(response).to be_successful
+            sortedMerchants = JSON.parse(response.body, symbolize_names: true)
+            expect((sortedMerchants[:data][0][:id]).to_s).to eq(@merchant3[:id].to_s)
+            expect((sortedMerchants[:data][2][:id]).to_s).to eq(@merchant1[:id].to_s)
+        end
+
+    describe "show" do
+        it "returns a single merchant" do
+
+        end
+    end
+
+    describe "#create" do
+        it "creates a new merchant" do 
+            merchant_params = {
+                name: "Joe"
+            }
+            headers = { "CONTENT_TYPE" => "application/json" }
+
+            post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
+
+            created_merchant = Merchant.last
+            
+            expect(response).to be_successful
+            expect(created_merchant.name).to eq("Joe")
+        end
         end
     end
 end
