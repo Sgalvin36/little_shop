78b68adba2a9b39d91365144f6788c26f25d1b4a
Shane Galvin <Sgalvin36@gmail.com>
2024-09-13 16:29:35 -0600

    Added additional cascade functionality for delete item

--- spec/requests/v1/items_request_spec.rb
+++ spec/requests/v1/items_request_spec.rb
@@ -142,11 +142,13 @@ describe "Items API" do
             
             
             expect(Item.count).to eq(5)
+            expect(InvoiceItem.count).to eq(2)
         
             delete "/api/v1/items/#{item.id}"
         
             expect(response).to be_successful
             expect(Item.count).to eq(4)
+            expect(InvoiceItem.count).to eq(0)
             expect{Item.find(item.id) }.to raise_error(ActiveRecord::RecordNotFound)
         end
     end
