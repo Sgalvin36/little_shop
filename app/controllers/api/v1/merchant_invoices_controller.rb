4a8895c4bd237ec50709cd69772238fb1f2da9e8
Shane Galvin <Sgalvin36@gmail.com>
2024-09-13 08:30:31 -0600

    Add filter functionality to invoice return

--- app/controllers/api/v1/merchant_invoices_controller.rb
+++ app/controllers/api/v1/merchant_invoices_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::MerchantInvoicesController < ApplicationController
     def index
-        # binding.pry 
-      @invoices = Invoice.where("merchant_id = #{params[:merchant_id]}")
-      render json: InvoiceSerializer.new(@invoices)
+        # binding.pry
+        @invoices = Invoice.where("merchant_id = ?" , "#{params[:merchant_id]}") 
+        if params[:status]
+            @invoices = Invoice.where("status = ?", "#{params[:status]}")
+        end
+            
+        render json: InvoiceSerializer.new(@invoices)
     end
 end
--- spec/requests/v1/merchant_invoices_spec.rb
+++ spec/requests/v1/merchant_invoices_spec.rb
@@ -6,22 +6,84 @@ describe "Merchant Invoices API" do
         @merchant = Merchant.create!(name: 'Test Merchant')
         @customer1 = create(:customer)
         @customer2 = create(:customer)
-        create_list(:invoice, 4, merchant_id: @merchant.id, status:"shipped", customer_id:@customer1.id)
-        create_list(:invoice, 4, merchant_id: @merchant.id, status:"returned", customer_id:@customer2.id)
+        create_list(:invoice, 2, merchant_id: @merchant.id, status:"shipped", customer_id:@customer1.id)
+        create_list(:invoice, 3, merchant_id: @merchant.id, status:"returned", customer_id:@customer2.id)
         create_list(:invoice, 4, merchant_id: @merchant.id, status:"packaged", customer_id:@customer1.id)
     end
 
-    it 'displays a list of all items' do
+    it 'displays a list of all invoices' do
         get "/api/v1/merchants/#{@merchant.id}/invoices"
 
         expect(response).to be_successful
 
         invoices = JSON.parse(response.body, symbolize_names: true)
 
+        expect(invoices[:data].length).to eq(9)
+
         invoices[:data].each do |invoice|
             expect(invoice[:id]).to be_a(String)
 
             expect(invoice[:attributes][:status]).to be_a(String)
+
+            expect(invoice[:attributes][:customer_id]).to be_a(Integer)
+
+            expect(invoice[:attributes][:merchant_id]).to eq(@merchant.id)
+        end
+    end
+
+    it 'displays a list of all packaged invoices' do
+        get "/api/v1/merchants/#{@merchant.id}/invoices?status=packaged"
+
+        expect(response).to be_successful
+
+        invoices = JSON.parse(response.body, symbolize_names: true)
+
+        expect(invoices[:data].length).to eq(4)
+        
+        invoices[:data].each do |invoice|
+            expect(invoice[:id]).to be_a(String)
+
+            expect(invoice[:attributes][:status]).to eq("packaged")
+
+            expect(invoice[:attributes][:customer_id]).to be_a(Integer)
+
+            expect(invoice[:attributes][:merchant_id]).to eq(@merchant.id)
+        end
+    end
+
+    it 'displays a list of all returned invoices' do
+        get "/api/v1/merchants/#{@merchant.id}/invoices?status=returned"
+
+        expect(response).to be_successful
+
+        invoices = JSON.parse(response.body, symbolize_names: true)
+
+        expect(invoices[:data].length).to eq(3)
+        
+        invoices[:data].each do |invoice|
+            expect(invoice[:id]).to be_a(String)
+
+            expect(invoice[:attributes][:status]).to eq("returned")
+
+            expect(invoice[:attributes][:customer_id]).to be_a(Integer)
+
+            expect(invoice[:attributes][:merchant_id]).to eq(@merchant.id)
+        end
+    end
+
+    it 'displays a list of all shipped invoices' do
+        get "/api/v1/merchants/#{@merchant.id}/invoices?status=shipped"
+
+        expect(response).to be_successful
+
+        invoices = JSON.parse(response.body, symbolize_names: true)
+
+        expect(invoices[:data].length).to eq(2)
+        
+        invoices[:data].each do |invoice|
+            expect(invoice[:id]).to be_a(String)
+
+            expect(invoice[:attributes][:status]).to eq("shipped")
 
             expect(invoice[:attributes][:customer_id]).to be_a(Integer)
 
