1d994bc87ef69c1aa419f313c3a6014d001fd8ea
Shane Galvin <Sgalvin36@gmail.com>
2024-09-13 15:21:18 -0600

    Merge update

---
+++ app/controllers/api/v1/merchant_invoices_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::MerchantInvoicesController < ApplicationController
+    def index
+        # binding.pry
+        @invoices = Invoice.where("merchant_id = ?" , "#{params[:merchant_id]}") 
+        if params[:status]
+            @invoices = @invoices.where("status = ?", "#{params[:status]}")
+        end
+            
+        render json: InvoiceSerializer.new(@invoices)
+    end
+end
