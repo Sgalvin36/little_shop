7d3b3f4fc537757b8f85625a33f5a60e112cb8ab
Joel Davalos <jsdavalos@gmail.com>
2024-09-14 14:19:42 -0600

    Added index functionality to merchant customer controller

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::MerchantCustomersController < ApplicationController
+    def index
+        merchant = Merchant.find(params[:merchant_id])
+        customers = merchant.customers
+        render json: CustomerSerializer.new(customers)
+    end
 end
