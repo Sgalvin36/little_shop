fc486d8b289b3897fc09250cc1c8a4b5713b3675
Shane Galvin <Sgalvin36@gmail.com>
2024-09-17 15:04:39 -0600

    Add sad path testing for invoices

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,8 +1,15 @@
 class ErrorSerializer
     def self.serialize(error, status)
-        {
-          "message": "Your status code is #{status}",
-          "errors": error.message.split(", ")
-        }
+      {
+        "message": "Your status code is #{status}",
+        "errors": error.message.split(", ")
+      }
+    end
+
+    def self.no_record_for_id
+      {
+        "message": "Your status code is 404",
+        "errors": "No records found for id"
+      }
     end
 end
