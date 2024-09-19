c3af56b01caf1117fcd85202167c9b53f7d854c7
Shane Galvin <Sgalvin36@gmail.com>
2024-09-19 08:39:11 -0600

    Added config to simple cov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start do
+  add_filter "spec/"
+  add_filter "config/"
+end
 ENV['RAILS_ENV'] ||= 'test'
-SimpleCov.start 'rails'
 require_relative '../config/environment'
 require 'support/factory_bot.rb'
 # Prevent database truncation if the environment is production
